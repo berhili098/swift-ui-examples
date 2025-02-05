360a1bfca354f65d90c89370d0c27234b00fdb89
oussama <oussamaberhili190@gmail.com>
2025-02-05 10:54:47 +0100

    Initial Commit

---
+++ navigationexample/navigationexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationexampleApp.swift
+//  navigationexample
+//
+//  Created by Oussama Berhili on 5/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct navigationexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
