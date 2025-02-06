bc3306c01398dd7e7003875547b82efbc140263f
oussama <oussamaberhili190@gmail.com>
2025-02-06 17:17:43 +0100

    Add new SwiftUI views: EnvironmentExampleView, UserViewModel, and ObserveObjectView

---
+++ navigationexample/Environment example/EnvironmentExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  EnvironmentExampleView.swift
+//  navigationexample
+//
+//  Created by Oussama Berhili on 6/2/2025.
+//
+
+import SwiftUI
+
+struct EnvironmentExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnvironmentExampleView()
+}
---
+++ navigationexample/Environment example/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  navigationexample
+//
+//  Created by Oussama Berhili on 6/2/2025.
+//
+
+import Foundation
